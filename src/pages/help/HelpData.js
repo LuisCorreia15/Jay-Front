3a4496543592dc48d2fb1c532aed0daf792af658
jordaoqualho <jordaoqualho@gmail.com>
2021-08-07 00:16:27 -0300

    add/search

---
+++ src/pages/help/HelpData.js
@@ -0,0 +1,22 @@
+export const HelpData = [
+  {
+    id: "001",
+    titulo: "Como fazer um orçamento?",
+  },
+  {
+    id: "002",
+    titulo: "Como fazer um aditivo?",
+  },
+  {
+    id: "003",
+    titulo: "Como fazer etiquetas pra vitrine?",
+  },
+  {
+    id: "004",
+    titulo: "Como conferir os pedidos?",
+  },
+  { id: "005", titulo: "Como fazer adicionar produtos?" },
+  { id: "006", titulo: "Como adicionar clientes?" },
+  { id: "007", titulo: "Como cancelar pedidos?" },
+  { id: "008", titulo: "Como fazer propaganda?" },
+];
--- src/pages/help/HelpPage.js
+++ src/pages/help/HelpPage.js
@@ -1,20 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import Menu from "components/menu/menu";
 import LoadingScreen from "components/loader/Loading";
-import { useHistory } from "react-router-dom";
-import "./HelpPage.css";
+import { HelpData } from "./HelpData";
 
 const HelpPage = () => {
   const history = useHistory();
+  const [termoDePesquisa, settermoDePesquisa] = useState("");
+  const [data, setData] = useState([{}]);
+
+  const helpData =
+    data.length === 0 ? (
+      <p>Nada encontrado!</p>
+    ) : (
+      data.map((row, i) => {
+        return (
+          <div
+            className="tb"
+            key={i}
+            onClick={() => {
+              history.push(`/help/${row.id}`);
+            }}
+          >
+            <div className="tb-title">
+              <h1>{row.titulo}</h1>
+            </div>
+          </div>
+        );
+      })
+    );
+
+  const doGetHelp = (termo) => {
+    let arrayX = new Array(HelpData.length);
+    if (termo === "") {
+      setData(HelpData);
+    } else {
+      for (let i = 0; i < HelpData.length; i++) {
+        let element = HelpData[i].titulo.toLowerCase();
+        if (element.indexOf(termo.toLowerCase()) > -1) {
+          arrayX[i] = HelpData[i];
+        }
+      }
+      setData(arrayX);
+    }
+  };
+
+  const handleSearchInputChange = async (event) => {
+    settermoDePesquisa(event.target.value);
+  };
+
+  useEffect(() => {
+    doGetHelp(termoDePesquisa);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [termoDePesquisa]);
 
   return (
     <>
       <LoadingScreen></LoadingScreen>
-      <div className="holder">
-        <Menu ativo="home"></Menu>
-        <div className="container">
-          <h1>Help Page</h1>
-        </div>
+      <Menu ativo=""></Menu>
+      <div className="container">
+        <form className="pd campo-busca">
+          <input
+            type="text"
+            value={termoDePesquisa}
+            placeholder="O que deseja buscar?"
+            autoFocus
+            onChange={handleSearchInputChange}
+          />
+        </form>
+        {helpData}
       </div>
     </>
   );
