22711abadfa92adbbe9d55fdd2bdaa976e247251
jordaoqualho <jordaoqualho@gmail.com>
2021-08-07 15:20:21 -0300

    add/start-quest001

--- src/pages/questions/Question001.js
+++ src/pages/questions/Question001.js
@@ -12,6 +12,26 @@ const Question001 = () => {
         <div className="q-title">
           <h1>Como fazer um orçamento?</h1>
         </div>
+        <div className="q-passo">
+          <h1>1. Verificar a data!</h1>
+          <p>Antes de fazer o orçamento, é necessário verificar se a data:</p>
+          <p>- Não é domingo ou segunda.</p>
+          <p>- Verificar se não tem muito pedido para o dia já.</p>
+        </div>
+        <div className="q-passo">
+          <h1>2. Verificar possibilidade do pedido!</h1>
+          <p>Para o pedido é necessário verificar:</p>
+          <p>
+            - Se é possivel fazer os doces no dia.
+            <span>(como cestinha de flores e doces caramelizados...)</span>
+          </p>
+
+          <p>- Se é possivel fazer decorados se houver.</p>
+          <p>
+            - Se for pouca quantidade cobrar preço da vitrine.
+            <span>(2 de cada por exemplo)</span>
+          </p>
+        </div>
       </div>
     </>
   );
